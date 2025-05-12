625eae4dae93085e491304e4e8b41d972cb41f16
oscarfeeler <oscarbo56@gmail.com>
2025-05-12 11:26:32 +0200

    se ha creado el script monitoreo.py el cual realiza funciones para mostrar el numero de procesos del sistema y su uso de CPU

--- ejercicio_parte_03/monitoreo.py
+++ ejercicio_parte_03/monitoreo.py
@@ -3,5 +3,21 @@ import psutil
 def mostrar_numero_procesos():
     num_procesos = len(psutil.pids())
     print(f"El número de procesos en ejecución es: {num_procesos}")
+    if num_procesos >= 100:
+        print("ALERTA: demasiados procesos")
     
-def mostrar__
+def mostrar_nombres_procesos():
+    procesos = []
+    for i in psutil.process_iter(['pid', 'name', 'cpu_percent']):
+        try:
+            procesos.append(i.info)
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue   
+    procesos_ordenados = sorted(procesos, key=lambda x: x['cpu_percent'], reverse=True)[:3]
+    print("Los 3 procesos que más CPU consumen son:")
+    for p in procesos_ordenados:
+        print(f"PID: {p['pid']}, Nombre: {p['name']}, CPU: {p['cpu_percent']}%")
+
+        
+mostrar_numero_procesos()
+mostrar_nombres_procesos()
